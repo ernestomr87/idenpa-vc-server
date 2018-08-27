e1c2a77e38e9b389ac933f6428c3174dae83b19e
Ernesto Miguel Rodriguez Rodriguez <ernestomr87@gmail.com>
2018-08-27 11:20:50 -0500

    update

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
-import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import logger from 'morgan';
 import indexRouter from './api/routes/index';
 
 let app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 
 const env = process.env.NODE_ENV || 'production';
 
 if (env === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, './src/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './src/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
   });
 }
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 export default app;
